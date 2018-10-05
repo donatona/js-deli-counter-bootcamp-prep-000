d71ec517403d3d46cb672a64e6f828e80a4ffae4
Tona Morales-Calkins <tona.mc@gmail.com>
2018-10-05 17:05:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(current, name) {
   current.push(name);
-  return "Welcome, " ${name}". You are number " current.length " in line.";
+  return "Welcome, "name.innerHTML". You are number " current.length " in line.";
   }
